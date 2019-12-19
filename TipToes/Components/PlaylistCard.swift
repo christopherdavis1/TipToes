925e55e100b9b4725f439569a6301ea937b02713
Christopher Davis <chris@christopheralandavis.com>
2019-12-18 22:36:57 -0500

    Created the playlist card, ready for data!

---
+++ TipToes/Components/PlaylistCard.swift
@@ -0,0 +1,126 @@
+//
+//  PlaylistCard.swift
+//  TipToes
+//
+//  Created by Christopher Davis on 12/18/19.
+//  Copyright © 2019 Curious Friends Club. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlaylistCard: View {
+    var body: some View {
+        Group {
+            
+            // Header Section
+            VStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        HStack {
+                            Image(systemName: "music.note")
+                                .font(.system(size: 14))
+                            Text("music")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                        }
+                        .foregroundColor(Color.white)
+                        .opacity(0.75)
+                        
+                        Spacer()
+                        
+                        Image("spotifyIcon")
+                    }
+                    .padding(.bottom, -8)
+                    
+                    Text("Playlists")
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                }
+                
+                // List of Playlists
+                VStack {
+                    
+                    // Single playlist row
+                    PlaylistItemRow(
+                        playlistTitle: "Chill Vibes",
+                        playlistLength: "42 minutes",
+                        playlistDescription: "Music from Tycho, Sleeping Awake and more."
+                    )
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                        .padding(.vertical, 8)
+                        .foregroundColor(.white)
+                        .opacity(0.25)
+                    
+                    PlaylistItemRow(
+                        playlistTitle: "Tunes 4 Flexin",
+                        playlistLength: "1hr 4min",
+                        playlistDescription: "Music from Avicii, Queen Bee, Troye Sivyan, and more. "
+                    )
+                }
+            }
+            
+        }
+        .padding(.horizontal)
+        .padding(.vertical)
+        .padding(.bottom, 8)
+        .background(Color("Green1"))
+        .frame(width: 342)
+        .cornerRadius(16)
+        .shadow(color: Color("GreenShadow"), radius: 4, x: 0, y: 2)
+    }
+}
+
+struct PlaylistCard_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistCard()
+    }
+}
+
+struct PlaylistItemRow: View {
+    
+    var playlistTitle: String
+    var playlistLength: String
+    var playlistDescription: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(playlistTitle)
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                        Image(systemName: "circle.fill")
+                            .font(.system(size: 4))
+                        Text(playlistLength)
+                            .font(.subheadline)
+                            .opacity(0.75)
+                    }
+                    
+                    Text(playlistDescription)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.top, -4)
+                    
+                }
+                .padding(.trailing, 4)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .padding(.vertical, 24)
+                    .padding(.horizontal, 10)
+                    .background(Color("GreenMidDark"))
+                    .cornerRadius(100)
+            }
+            .foregroundColor(Color.white)
+            
+            //            Rectangle()
+            //                .frame(height: 1)
+            //                .padding(.vertical, 8)
+            //                .foregroundColor(.white)
+            //                .opacity(0.25)
+        }
+    }
+}
--- TipToes/Views/HorizontalQuoteScroll.swift
+++ TipToes/Views/HorizontalQuoteScroll.swift
@@ -10,20 +10,32 @@ import SwiftUI
 
 struct HorizontalQuoteScroll: View {
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
+        VStack {
             
-            HStack(alignment: .top, spacing: 16.0) {
-                QuoteCardComponent(quoteImage: "AlanWatts1", quoteBody: "No valid plans for the future can be made by those who have no capacity for living now.", quoteAttribution: "Alan Watts")
+            Spacer()
+            
+            ScrollView(.horizontal, showsIndicators: false) {
                 
-                QuoteCardComponent(quoteImage: "AlanWatts2", quoteBody: "What the devil is the point of surviving, going on living, when it's a drag? But you see, that's what people do.", quoteAttribution: "Alan Watts")
-                
-                QuoteCardComponent(quoteImage: "AlanWatts3", quoteBody: "The reason we want to go on and on is because we live in an impoverished present.", quoteAttribution: "Alan Watts")
-                
-                QuoteCardComponent(quoteImage: "AlanWatts2", quoteBody: "What the devil is the point of surviving, going on living, when it's a drag? But you see, that's what people do.", quoteAttribution: "Alan Watts")
+                HStack(alignment: .top, spacing: 16.0) {
+                    QuoteCardComponent(quoteImage: "AlanWatts1", quoteBody: "No valid plans for the future can be made by those who have no capacity for living now.", quoteAttribution: "Alan Watts")
+                    
+                    QuoteCardComponent(quoteImage: "AlanWatts2", quoteBody: "What the devil is the point of surviving, going on living, when it's a drag? But you see, that's what people do.", quoteAttribution: "Alan Watts")
+                    
+                    QuoteCardComponent(quoteImage: "AlanWatts3", quoteBody: "The reason we want to go on and on is because we live in an impoverished present.", quoteAttribution: "Alan Watts")
+                    
+                    QuoteCardComponent(quoteImage: "AlanWatts2", quoteBody: "What the devil is the point of surviving, going on living, when it's a drag? But you see, that's what people do.", quoteAttribution: "Alan Watts")
+                }
+                .padding()
+                .padding(.bottom, 16)
             }
-            .padding()
-            .padding(.bottom, 16)
+            
+            PlaylistCard()
+            
+            Spacer()
+
         }
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
